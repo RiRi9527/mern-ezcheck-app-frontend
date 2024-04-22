1dbc0325ecb17f080f30846d84b62f2ee0e8ebf6
Hao Zhou <hzhou129527@gmail.com>
2024-04-21 20:30:18 -0400

    updated some react-big-calendar

--- src/big-react-calendar/big-react-calender.tsx
+++ src/big-react-calendar/big-react-calender.tsx
@@ -1,41 +1,128 @@
-import "react-big-calendar/lib/css/react-big-calendar.css";
-
 import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
 
 // Setup the localizer by providing the moment (or globalize, or Luxon) Object
 // to the correct localizer.
 const localizer = momentLocalizer(moment); // or globalizeLocalizer
 
-const MyCalendar = () => (
-  <div className="myCustomHeight">
-    <Calendar
-      localizer={localizer}
-      events={myEventsList}
-      startAccessor="start"
-      endAccessor="end"
-      min={new Date(0, 0, 0, 8)} // 设置日历开始时间为上午 8:00
-      max={new Date(0, 0, 0, 23)} // 设置日历结束时间为晚上 8:00
-    />
-  </div>
-);
+const MyCalendar = () => {
+  const generateWorkHours = () => {
+    const startOfWeek = moment().startOf("week"); // Start date of the week
+    const endOfWeek = moment().endOf("week"); // End date of the week
 
+    const workHours = [];
+
+    for (let day = startOfWeek; day <= endOfWeek; day.add(1, "day")) {
+      if (day.isoWeekday() >= 1 && day.isoWeekday() <= 5) {
+        // Only handle Monday to Friday
+        const start = day.clone().hour(9).toDate(); // 9:00 AM
+        const end = day.clone().hour(17).toDate(); // 5:00 PM
+
+        workHours.push({
+          title: "Work Hours",
+          start,
+          end,
+          allDay: false,
+          backgroundColor: "rgba(0, 255, 0, 0.3)", // need fix - not working now
+        });
+      }
+    }
+
+    return workHours;
+  };
+  const backgroundEvents = generateWorkHours();
+
+  const eventStyleGetter = (
+    event: any,
+    start: Date,
+    end: Date
+    // isSelected: boolean
+  ) => {
+    if (event.title === "Lunch Break") {
+      const time = end.getTime() - start.getTime();
+      if (time <= 30 * 60 * 1000) {
+        return {
+          style: {
+            backgroundColor: "rgba(0, 255, 0, 0.7)", // Green color with 30% opacity
+          },
+        };
+      }
+      if (time > 30 * 60 * 1000) {
+        return {
+          style: {
+            backgroundColor: "rgba(255, 0, 0, 0.3)", // Red color with 30% opacity
+          },
+        };
+      }
+    }
+
+    if (event.title === "Actual Time") {
+      const time = end.getTime() - start.getTime();
+      if (time >= 8 * 1000 * 60 * 60) {
+        return {
+          style: {
+            backgroundColor: "rgba(0, 255, 0, 0.7)", // Green color with 30% opacity
+          },
+        };
+      }
+      if (time < 7.83 * 1000 * 60 * 60) {
+        return {
+          style: {
+            backgroundColor: "rgba(255, 0, 0, 0.7)", // Red color with 30% opacity
+          },
+        };
+      }
+    }
+
+    return {};
+  };
+
+  return (
+    <div className="myCustomHeight" style={{ height: 600 }}>
+      <Calendar
+        // dayLayoutAlgorithm="no-overlap"
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        min={new Date(0, 0, 0, 8)} // Set calendar start time to 8:00 AM
+        max={new Date(0, 0, 0, 23)} // Set calendar end time to 8:00 PM
+        defaultView="work_week"
+        views={{
+          month: true,
+          week: true,
+          work_week: true,
+          day: true,
+          agenda: true,
+        }}
+        backgroundEvents={backgroundEvents} // Set background events
+        eventPropGetter={eventStyleGetter}
+      />
+    </div>
+  );
+};
 export default MyCalendar;
 
 const myEventsList = [
   {
-    title: "Schedule Time",
-    start: new Date(2024, 3, 17, 9, 0), // April 17, 2024, 10:00 AM
-    end: new Date(2024, 3, 17, 17, 0), // April 17, 2024, 11:00 AM
+    title: "Actual Time",
+    start: new Date(2024, 3, 22, 9, 0), // April 17, 2024, 9:00 AM
+    end: new Date(2024, 3, 22, 17, 15), // April 17, 2024, 5:15 PM
   },
   {
     title: "Lunch Break",
-    start: new Date(2024, 3, 17, 12, 0), // April 17, 2024, 12:00 PM
-    end: new Date(2024, 3, 17, 12, 28), // April 17, 2024, 1:00 PM
+    start: new Date(2024, 3, 22, 12, 0), // April 17, 2024, 12:00 PM
+    end: new Date(2024, 3, 22, 12, 28), // April 17, 2024, 1:00 PM
   },
   {
     title: "Actual Time",
-    start: new Date(2024, 3, 17, 9, 0), // April 17, 2024, 10:00 AM
-    end: new Date(2024, 3, 17, 17, 15), // April 17, 2024, 11:00 AM
+    start: new Date(2024, 3, 23, 9, 0), // April 17, 2024, 9:00 AM
+    end: new Date(2024, 3, 23, 16, 49), // April 17, 2024, 5:15 PM
+  },
+  {
+    title: "Lunch Break",
+    start: new Date(2024, 3, 23, 12, 0), // April 17, 2024, 12:00 PM
+    end: new Date(2024, 3, 23, 12, 31), // April 17, 2024, 1:00 PM
   },
 ];
