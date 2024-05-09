ec24656425765dfd5ec562d3a5c2eaf6387c593c
Hao Zhou <hzhou129527@gmail.com>
2024-05-08 23:11:54 -0400

    start rebuild user-page and change name to MainPage

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,11 +4,13 @@ import HomePage from "./pages/HomePage";
 import UserProfileAdminPage from "./pages/UserProfileAdminPage";
 
 import ProtectedRoute from "./auth/ProtectedRoute";
+import MainPage from "./pages/MainPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/main" element={<MainPage />} />
 
       <Route element={<ProtectedRoute />}>
         <Route
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,32 @@
+const MainPage = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="bg-gray-700 h-11 flex justify-between">
+        <div className="flex flex-row w-1/2">
+          <div className="flex items-center">
+            <img src="" alt="Logo" />
+          </div>
+          <div className="px-2 my-1 border-b border-gray-400 flex-1 max-w-[450px] flex items-center">
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-transparent outline-none w-full text-white"
+            />
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="bg-blue-900 rounded-full w-9 h-9 flex justify-center items-center">
+            <h1 className="text-white font-bold">HZ</h1>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row w-full flex-1">
+        <div className="w-9 bg-gray-700 ">left</div>
+        <div className="flex-1 bg-red-500">right</div>
+      </div>
+      <div className="h-3 bg-gray-700"></div>
+    </div>
+  );
+};
+
+export default MainPage;
