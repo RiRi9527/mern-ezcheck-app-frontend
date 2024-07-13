709c18d83c9a2235a073571c8d6b7ac1f477b628
Hao Zhou <hzhou129527@gmail.com>
2024-07-13 18:21:12 -0400

    keep working on payroll

--- src/pages/PayrollPage.tsx
+++ src/pages/PayrollPage.tsx
@@ -1,4 +1,17 @@
+import { usePayroll } from "@/api/EventApi";
+import { useAppContext } from "@/content/AppContext";
+
 const PayrollPage = () => {
+  const { user } = useAppContext();
+
+  const { payroll } = usePayroll(
+    user?._id,
+    "2024-07-13T12:34:56.789Z",
+    "payroll"
+  );
+
+  console.log(payroll);
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="bg-white shadow-lg p-8 w-[8.27in] h-[11.69in] border border-gray-300">
